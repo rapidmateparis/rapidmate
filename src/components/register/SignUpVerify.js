45cc39c38580f35762b03a4562a9c96cd4944034
DHANASEKARAN <dhanasekar@DHANASEKARANs-MacBook-Pro.local>
2024-07-30 20:46:45 +0530

    signup Verficaiton completed

--- src/components/register/SignUpVerify.js
+++ src/components/register/SignUpVerify.js
@@ -36,41 +36,33 @@ const SignUpVerify = ({navigation}) => {
       signUpVerifyApi(params, (successResponse) => {
         setLoading(false)
         if(successResponse[0]._success){
-          if(successResponse[0]._response) {
-            if(successResponse[0]._response.name == 'NotAuthorizedException') {
-              Alert.alert('Error Alert', successResponse[0]._response.name, [
-                {text: 'OK', onPress: () => {}},
-              ]);
-            } else {
-            let loginParams = {
-              info: {
-                userName: signUpDetails.userName,
-                password: signUpDetails.password,
-              }
-            };
-            setLoading(true)
-            authenticateUser(loginParams, (successResponse) => {
-              setLoading(false)
-              if(successResponse[0]._success){
-                if(successResponse[0]._response) {
-                  if(successResponse[0]._response.name == 'NotAuthorizedException') {
-                    Alert.alert('Error Alert', successResponse[0]._response.name, [
-                      {text: 'OK', onPress: () => {}},
-                    ]);
-                  } else {
-                    saveUserDetails(successResponse[0]._response.idToken.payload);
-                    navigation.navigate('PickupBottomNav');
-                  }
+          let loginParams = {
+            info: {
+              userName: signUpDetails.userName,
+              password: signUpDetails.password,
+            }
+          };
+          setLoading(true)
+          authenticateUser(loginParams, (successResponse) => {
+            setLoading(false)
+            if(successResponse[0]._success){
+              if(successResponse[0]._response) {
+                if(successResponse[0]._response.name == 'NotAuthorizedException') {
+                  Alert.alert('Error Alert', successResponse[0]._response.name, [
+                    {text: 'OK', onPress: () => {}},
+                  ]);
+                } else {
+                  saveUserDetails({userInfo : successResponse[0]._response.user.idToken.payload, userDetails: successResponse[0]._response.user_profile});
+                  navigation.navigate('PickupBottomNav');
                 }
               }
-            }, (errorResponse)=> {
-              setLoading(false)
-              Alert.alert('Error Alert', errorResponse, [
-                {text: 'OK', onPress: () => {}},
-              ]);
-            })
-          }
-        }
+            }
+          }, (errorResponse)=> {
+            setLoading(false)
+            Alert.alert('Error Alert', errorResponse, [
+              {text: 'OK', onPress: () => {}},
+            ]);
+          })
         }
       }, (errorResponse)=> {
         setLoading(false)
