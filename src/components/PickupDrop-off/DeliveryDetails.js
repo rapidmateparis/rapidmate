8e4bad5b7c4e993bd384041996cbcb3b85fca347
Saurabh Mishra <saurabh88mishra>
2024-11-25 11:26:17 +0530

    Otp Changes

--- src/components/PickupDrop-off/DeliveryDetails.js
+++ src/components/PickupDrop-off/DeliveryDetails.js
@@ -499,15 +499,21 @@ const DeliveryDetails = ({navigation, route}) => {
             Vehicle:
             <Text style={styles.detailsId}> {vehicleType.vehicle_type}</Text>
           </Text>
-          {route.params?.orderItem?.delivered_otp && (
-            <Text style={styles.orderdetails}>
-              Delivered OTP:
-              <Text style={styles.detailsId}>
-                {' '}
-                {route.params?.orderItem?.delivered_otp}
-              </Text>
+          <Text style={styles.orderdetails}>
+            OTP:
+            <Text style={styles.detailsId}>
+              {' '}
+              {route.params?.orderItem?.otp}
             </Text>
-          )}
+          </Text>
+
+          <Text style={styles.orderdetails}>
+            Delivered OTP:
+            <Text style={styles.detailsId}>
+              {' '}
+              {route.params?.orderItem?.delivered_otp}
+            </Text>
+          </Text>
         </View>
 
         <TouchableOpacity
