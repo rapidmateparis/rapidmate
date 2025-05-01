a0cc2e47b4101894893247244be72d4f27512bf3
syszoo <support@syszoo.com>
2025-05-01 15:42:25 +0530

    Changes the port

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -22,9 +22,9 @@ export const HTTPMethod = {
 };
 
 export const PORT = {
-  qa: '3009',
-  uat: '3041',
-  prod: '3906',
+  qa: '3002',
+  uat: '3005',
+  prod: '3909',
 };
 
 export const BASE_URL = `http://16.171.224.246:${PORT.qa}/api/`;
