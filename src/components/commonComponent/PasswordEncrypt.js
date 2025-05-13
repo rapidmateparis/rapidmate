40dd26a396f113e8b75084badec1737ed25dbcc8
AreshNatarajan <areshn18@gmail.com>
2025-05-13 17:50:19 +0530

    encryption issue fixed used  version 3.1.9-1 crypto-js

--- src/components/commonComponent/PasswordEncrypt.js
+++ src/components/commonComponent/PasswordEncrypt.js
@@ -1,39 +1,44 @@
-// crypto-js-encryption.js
 import CryptoJS from 'crypto-js';
 
-// AES-128 key (16 characters = 128 bits)
-const SECRET_KEY = 'R@p1dmat3@2025$';
+// console.log("Console log :::::::::::::;;;for crypto", CryptoJS)
 
+const SECRET_KEY = 'R@p1dmat3@2025$20250525023532345'; // Must be 16 chars for AES-128
+
+// Encrypt function
 export function encrypt(text) {
-  const key = CryptoJS.enc.Utf8.parse(SECRET_KEY);
-  const iv = CryptoJS.lib.WordArray.random(16); // 16-byte random IV
+  // const key = CryptoJS.enc.Utf8.parse(SECRET_KEY);
+  // const iv = CryptoJS.lib.WordArray.random(16); // AES block size = 16 bytes
+
+  const key = CryptoJS.enc.Utf8.parse(SECRET_KEY); // 32 chars
+  const iv = CryptoJS.enc.Utf8.parse('1234567890123456'); // 16 chars
 
   const encrypted = CryptoJS.AES.encrypt(text, key, {
-    iv: iv,
+    iv,
     mode: CryptoJS.mode.CBC,
     padding: CryptoJS.pad.Pkcs7,
   });
 
-  // Return hex representations
-  return {
-    iv: iv.toString(CryptoJS.enc.Hex),
-    encryptedData: encrypted.ciphertext.toString(CryptoJS.enc.Hex),
-  };
+  return CryptoJS.enc.Base64.stringify(encrypted.ciphertext);
+
+  // return {
+  //   iv: iv.toString(CryptoJS.enc.Hex),
+  //   encryptedData: encrypted.ciphertext.toString(CryptoJS.enc.Hex),
+  // };
 }
 
+// Decrypt function
 export function decrypt(encryptedHex, ivHex) {
   const key = CryptoJS.enc.Utf8.parse(SECRET_KEY);
   const iv = CryptoJS.enc.Hex.parse(ivHex);
   const encrypted = CryptoJS.enc.Hex.parse(encryptedHex);
 
-  // Convert to Base64 string to match AES.decrypt input format
   const encryptedBase64 = CryptoJS.enc.Base64.stringify(encrypted);
 
   const decrypted = CryptoJS.AES.decrypt(encryptedBase64, key, {
-    iv: iv,
+    iv,
     mode: CryptoJS.mode.CBC,
     padding: CryptoJS.pad.Pkcs7,
   });
 
-  return decrypted.toString(CryptoJS.enc.Utf8);
+  return decrypted.toString(CryptoJS.enc.Utf8); // Returns original plaintext
 }
